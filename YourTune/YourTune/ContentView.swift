ed4cdceddd8b9fca3d996bba358df18fc2e8ebca
BekaL <beka75610@gmail.com>
2025-01-11 14:45:25 +0400

    Initial Commit

---
+++ YourTune/YourTune/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YourTune
+//
+//  Created by Beka on 11.01.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YourTune/YourTune/YourTuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourTuneApp.swift
+//  YourTune
+//
+//  Created by Beka on 11.01.25.
+//
+
+import SwiftUI
+
+@main
+struct YourTuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
